8cf14cbd77ea320753ef6f6767096c2cb5e58a57
Daniel082811 <rysbekovicdaniel@gmail.com>
2024-10-09 18:35:42 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// const city = ['bishkek', 'shaary', 'Kyrgisztanda', 'al',
+//  'objast', '7','bar', 'borborubuz', 'ayabay', ]
+
+// console.log(city);
+// console.log(city  [2], city[6].num, city[4], city[7], city[8], city[0], city[3], city[9],city[5]);
+
+
+// const history ={
+//     book: ['пушкин',6,  'жазган',  'китеп'],
+// read: 'fasle',
+// info: 'lorem'
+
+// }
+
+// console.log(history.book[0] ,  history.book[1].history.book[3],history.book[2]); 
+// console.log(history.read);
+// console.log(history.info);
+
+
+// if ('eckjdbt') {
+// // код true
+// } else {
+//      //rjl  false
+
+// }
+
+const num = prompt('Погода')
+if (num>30) {
+ console.log(`${num} кун ысык`);   
+}
+else if (num<25) {
+    console.log(`${num} кун суук`);
+}
+
+
+
+
